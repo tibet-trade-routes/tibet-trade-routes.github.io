28cf399103d39ca44757019cb8f5fb1f123549ca
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-12-14 22:18:04 -0500

    add controls

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+const weightRange = [2, 10];
+const yearRange = [1900, 2024];
+
 const locationsMap = require('./data/locations.js');
 const locationsLayer = [];
 for (const [name, data] of Object.entries(locationsMap)) {
@@ -29,10 +32,9 @@ for (const [name, color, locations] of ryavecPaths) {
     locations.map(location => [locationsMap[location][0], locationsMap[location][1]]),
     {
       delay: 1000,
-      dashArray: [10, 30],
+      dashArray: [0, 20],
       hardwareAccelerated: true,
-      weight: 7,
-      fillOpacity: 1,
+      weight: weightRange[1],
       color: 'black',
       pulseColor: '#FFFFFF',
       paused: false,
@@ -50,9 +52,9 @@ for (const [name, color, locations] of crhPaths) {
     locations.map(location => [locationsMap[location][0], locationsMap[location][1]]),
     {
       delay: 1000,
-      dashArray: [10, 30],
+      dashArray: [0, 20],
       hardwareAccelerated: true,
-      weight: 7,
+      weight: weightRange[0],
       color: 'green',
       pulseColor: '#FFFFFF',
       paused: false,
@@ -65,3 +67,46 @@ for (const [name, color, locations] of crhPaths) {
 L.layerGroup(ryavecLayer).addTo(map);
 L.layerGroup(crhLayer).addTo(map);
 L.layerGroup(locationsLayer).addTo(map);
+
+function updateTradeRoutes (year) {
+  let weight = (yearRange[1] - year) / (yearRange[1] - yearRange[0]) * (weightRange[1] - weightRange[0]) + weightRange[0];
+  for (const antPath of ryavecLayer) {
+    // change weight of antPath based on year
+    antPath.setStyle({ weight });
+  }
+  weight = (year - yearRange[0]) / (yearRange[1] - yearRange[0]) * (weightRange[1] - weightRange[0]) + weightRange[0];
+  for (const antPath of crhLayer) {
+    // change weight of antPath based on year
+    antPath.setStyle({ weight });
+  }
+}
+
+document.getElementById('play').addEventListener('click', play);
+document.getElementById('pause').addEventListener('click', pause);
+
+document.getElementById('slider').addEventListener('input', event => {
+  const year = event.target.value;
+  updateTradeRoutes(year);
+  document.getElementById('year-display').innerText = year;
+});
+
+let currentInterval = null;
+let currentYear = 1900;
+
+function pause () {
+  clearInterval(currentInterval);
+}
+
+function play () {
+  currentInterval = setInterval(() => {
+    currentYear += 1;
+    if (currentYear > yearRange[1]) {
+      currentYear = yearRange[0];
+    }
+    updateTradeRoutes(currentYear);
+    document.getElementById('year-display').innerText = currentYear;
+    document.getElementById('slider').value = currentYear;
+  }, 20);
+}
+
+play();
