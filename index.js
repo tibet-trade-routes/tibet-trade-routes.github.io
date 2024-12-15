dba1350542d6422b0635aba1ad75482f57ad949e
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-12-14 22:32:31 -0500

    move public/ -> docs/

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${3000}`);
